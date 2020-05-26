1ca6f85a90fa9b940d1f9ce6291e4307be0866aa
chenyang24 <chenyang24@58.com>
2020-05-26 23:37:22 +0800

    图片问题
    Signed-off-by: chenyang24 <chenyang24@58.com>

---
+++ src/main/java/io/pana/juc/chapter01/Volatile.java
@@ -0,0 +1,38 @@
+package io.pana.juc.chapter01;
+
+public class Volatile {
+
+    private int i = 0;
+
+    private boolean flag = false;
+
+    private void writeIValue(){
+        i = 2889;
+        flag = true;
+    }
+
+    private void readIValue(){
+        while(!flag){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) throws Exception{
+        Volatile v = new Volatile();
+        Thread t1 = new Thread(v::writeIValue);
+        Thread t2 = new Thread(v::readIValue);
+        Thread t3 = new Thread(v::readIValue);
+
+        t2.start();
+        t3.start();
+
+        Thread.sleep(1000);
+        t1.start();
+
+    }
+}
